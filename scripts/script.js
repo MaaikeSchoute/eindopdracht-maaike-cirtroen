575ed486acd024fc410234f8dbd89ed1aa499ad7
MaaikeSchoute <maaike.schoute2@hva.nl>
2024-05-30 09:32:47 +0200

    kleuren achtergrond. Automatisch afbeelding loop. Geluidje met slaan

--- scripts/script.js
+++ scripts/script.js
@@ -7,35 +7,41 @@ const getalEl = document.querySelector("#getal");
 const header = document.querySelector("header");
 const main = document.querySelector("main");
 const footer = document.querySelector("footer");
+const mepGeluid = document.getElementById("mepGeluid");
 
-// --------------------------------------- Variablenen ---------------------------------------
+// --------------------------------------- Variabelen ---------------------------------------
 let plaatjesArray = ['img/frame/vlieg.png','img/frame/kat.png','img/frame/dolfijn.png','img/frame/hond.png','img/frame/kip.png','img/frame/konijn.png','img/frame/olifant.png','img/frame/paard.png','img/frame/schaap.png','img/frame/schaap.png','img/frame/slang.png','img/frame/vogel.png'];
-let plaatjesArrayFout = ['img/frame/dead/vlieg.png','img/frame/dead/kat.png','img/frame/dead/dolfijn.png','img/frame/dead/hond.png','img/frame/dead/kip.png','img/frame/dead/konijn.png','img/frame/dead/olifant.png','img/frame/dead/paard.png','img/frame/dead/schaap.png','img/frame/dead/schaap.png','img/frame/dead/slang.png','vogel.png'];
+let plaatjesArrayFout = ['img/frame/dead/vlieg.png','img/frame/dead/kat.png','img/frame/dead/dolfijn.png','img/frame/dead/hond.png','img/frame/dead/kip.png','img/frame/dead/konijn.png','img/frame/dead/olifant.png','img/frame/dead/paard.png','img/frame/dead/schaap.png','img/frame/dead/schaap.png','img/frame/dead/slang.png','img/frame/dead/vogel.png'];
 let randomDierNummer;
-let secondsPassed = 0;
 let getal = 0;
+let aantalGemepteVliegen = document.querySelector("#getal");
+let intervalId;
+let intervalRunning = false;
 
-//--------------------------------------- Funtions ---------------------------------------
-
+//--------------------------------------- Functies ---------------------------------------
 function startSpel() {
-    randomDierNummer = Math.floor(Math.random()*11);
-    console.log("random dier nummer: "+ randomDierNummer);
+    randomDierNummer = Math.floor(Math.random() * plaatjesArray.length);
+    console.log("random dier nummer: " + randomDierNummer);
     dierPlaatje.src = plaatjesArray[randomDierNummer];
     resetBackgrounds();
 }
 
 function mepDier() {
+    clearInterval(intervalId); // Stop het interval
     dierPlaatje.src = plaatjesArrayFout[randomDierNummer];
+    mepGeluid.play();
+    dierPlaatje.classList.add("mepAnimatie");
 
     if (randomDierNummer === 0) {
         console.log("goed");
         main.classList.add("bgGoed");
         footer.classList.add("bgGoedHF");
         header.classList.add("bgGoedHF");
-        
-    }
 
-    else {
+        // Update de score
+        getal++;
+        aantalGemepteVliegen.textContent = getal;
+    } else {
         console.log("fout");
         main.classList.add("bgFoutBody");
         footer.classList.add("bgFoutHF");
@@ -44,30 +50,21 @@ function mepDier() {
 }
 
 function resetBackgrounds() {
-    startKnop.addEventListener("click", startSpel);
     main.classList.remove("bgFoutBody", "bgGoed");
     footer.classList.remove("bgFoutHF", "bgGoedHF");
     header.classList.remove("bgFoutHF", "bgGoedHF");
 }
 
-startKnop.addEventListener("click", startSpel);
-dierPlaatje.addEventListener("click", mepDier);
-
- /*function stop() {
-    clearInterval(intervalId);
-    let randomDierNummer = Math.floor(Math.random()*11);
-    dierPlaatje.src = plaatjesArrayFout[randomDierNummer];
+function startAutoPlay() {
+    if (!intervalRunning) {
+        intervalId = setInterval(startSpel, 1000); // Verander elke seconde
+        intervalRunning = true;
+    }
 }
 
-
-  if (randomDierNummer == plaatjesArray '0') {
-        document.body.classList.add("goed");
-        document.dierPlaatje("src=")
-        getal += 1;
-    }*/
-//--------------------------------------- text content veranderingen ---------------------------------------
-
-// --------------------------------------- uitvoering ---------------------------------------
-
-
-
+//--------------------------------------- Event Listeners ---------------------------------------
+// Knop "Volgend dier"
+startKnop.addEventListener("click", function() {
+    startAutoPlay();
+});
+dierPlaatje.addEventListener("click", mepDier);
