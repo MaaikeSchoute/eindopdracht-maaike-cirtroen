dec7b7061fe89b0fd19ac300a6641f2cdc4b6024
MaaikeSchoute <maaike.schoute2@hva.nl>
2024-05-30 11:37:22 +0200

    Heel veel veranderingen. Moeilijkheids knoppen. Hidden knoppen

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,24 @@
 console.log("hello");
 
 //--------------------------------------- Constanten ---------------------------------------
+// HTML elementen
 const startKnop = document.querySelector("#knop");
 const dierPlaatje = document.querySelector("#dierPlaatje");
 const getalEl = document.querySelector("#getal");
 const header = document.querySelector("header");
 const main = document.querySelector("main");
 const footer = document.querySelector("footer");
+// geluids effecten
 const mepGeluid = document.querySelector("#mepGeluid");
-const spelGeluid = document.querySelector("#spelGeluid")
+const spelGeluid = document.querySelector("#spelGeluid");
+const winGeluid = document.querySelector("#winGeluid");
+const failGeluid = document.querySelector("#failGeluid");
+// sneleheidsknoppen
+const makkelijkKnop = document.querySelector("#makkelijk");
+const normaalKnop = document.querySelector("#normaal");
+const moeilijkKnop = document.querySelector("#moeilijk");
+const expertKnop = document.querySelector("#expert");
+const snelheidsKnoppen = document.querySelector("#snelheidsKnoppen");
 
 // --------------------------------------- Variabelen ---------------------------------------
 let plaatjesArray = ['img/frame/vlieg.png','img/frame/kat.png','img/frame/dolfijn.png','img/frame/hond.png','img/frame/kip.png','img/frame/konijn.png','img/frame/olifant.png','img/frame/paard.png','img/frame/schaap.png','img/frame/schaap.png','img/frame/slang.png','img/frame/vogel.png'];
@@ -18,6 +28,7 @@ let getal = 0;
 let aantalGemepteVliegen = document.querySelector("#getal");
 let intervalId;
 let intervalRunning = false;
+let snelheid = 1000;
 
 //--------------------------------------- Functies ---------------------------------------
 function startSpel() {
@@ -25,12 +36,17 @@ function startSpel() {
     console.log("random dier nummer: " + randomDierNummer);
     dierPlaatje.src = plaatjesArray[randomDierNummer];
     resetBackgrounds();
+    spelGeluid.play();
 }
 
 function mepDier() {
+    snelheidsKnoppen.classList.toggle("hidden");
     clearInterval(intervalId); // Stop het interval
+    intervalRunning = false;
+    console.log(intervalId);
     dierPlaatje.src = plaatjesArrayFout[randomDierNummer];
     mepGeluid.play();
+    spelGeluid.pause();
     dierPlaatje.classList.add("mepAnimatie");
 
     if (randomDierNummer === 0) {
@@ -38,6 +54,7 @@ function mepDier() {
         main.classList.add("bgGoed");
         footer.classList.add("bgGoedHF");
         header.classList.add("bgGoedHF");
+        winGeluid.play();
 
         // Update de score
         getal++;
@@ -47,6 +64,7 @@ function mepDier() {
         main.classList.add("bgFoutBody");
         footer.classList.add("bgFoutHF");
         header.classList.add("bgFoutHF");
+        failGeluid.play();
     }
 }
 
@@ -57,15 +75,38 @@ function resetBackgrounds() {
 }
 
 function startAutoPlay() {
+    console.log("interval running: " + intervalRunning);
     if (!intervalRunning) {
-        intervalId = setInterval(startSpel, 1000); // Verander elke seconde
+        intervalId = setInterval(startSpel, snelheid); // Verander elke seconde
         intervalRunning = true;
     }
 }
 
+function setMoeilijkheid(niveau) {
+
+    snelheid = niveau;
+    console.log("niveau: " + snelheid);
+    clearInterval(intervalId);
+    intervalRunning = false;
+    startAutoPlay();
+
+}
 //--------------------------------------- Event Listeners ---------------------------------------
 // Knop "Volgend dier"
 startKnop.addEventListener("click", function() {
     startAutoPlay();
 });
 dierPlaatje.addEventListener("click", mepDier);
+
+makkelijkKnop.addEventListener("click", function() {
+    setMoeilijkheid(2000); // 2 seconds
+});
+normaalKnop.addEventListener("click", function() {
+    setMoeilijkheid(1000); // 1 second
+});
+moeilijkKnop.addEventListener("click", function() {
+    setMoeilijkheid(750); // 0.75 seconds
+});
+expertKnop.addEventListener("click", function() {
+    setMoeilijkheid(500); // 0.5 seconds
+});
